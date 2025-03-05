6a86e46a78c42d055081e2f4a49f9754b39830a4
Toth-Mate <t0thm4t3@gmail.com>
2025-03-05 14:16:10 +0100

    Navigációs menü

--- idopont-foglalas/src/App.vue
+++ idopont-foglalas/src/App.vue
@@ -1,16 +1,13 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
+import { RouterView } from 'vue-router'
+import Navbar from '@/components/Navbar.vue'
 </script>
 
 <template>
-  <header>
-    <div class="wrapper">
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+  <div class="container">
+    <header>
+      <Navbar />
+    </header>
+    <RouterView />
+  </div>
 </template>
