34df1d6eb9f8ba865ed517a47bdd6fa6277c9145
Toth-Mate <t0thm4t3@gmail.com>
2025-03-05 14:24:08 +0100

    Pinia store az időpontoknak

---
+++ idopont-foglalas/src/stores/appointments.js
@@ -0,0 +1,26 @@
+import { ref, computed } from 'vue'
+import { defineStore } from 'pinia'
+
+import AppointmentService from '@/services/AppointmentService'
+
+export const useAppointmentStore = defineStore('appointment', () => {
+  const appointments = ref([])
+
+  async function getAppointments() {
+    try {
+      appointments.value = await AppointmentService.index()
+    } catch(e) {
+      console.log(e)
+    }
+  }
+
+  async function saveNewAppointment(appointment) {
+    try {
+      await AppointmentService.insert(appointment)
+    } catch(e) {
+      console.log(e)
+    }
+  }
+
+  return { appointments, getAppointments, saveNewAppointment }
+})
