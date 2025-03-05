d3107c2aa82a96c2b90907317e41a790165f8286
Toth-Mate <t0thm4t3@gmail.com>
2025-03-05 14:44:26 +0100

    Bootstrap - menü működik reszponzívan

--- idopont-foglalas/src/main.js
+++ idopont-foglalas/src/main.js
@@ -1,5 +1,5 @@
-//import './assets/main.css'
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import '../node_modules/bootstrap/dist/js/bootstrap.min.js'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
